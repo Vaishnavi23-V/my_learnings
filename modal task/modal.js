84ad7ba3ef391de96775e868db7e21e9bb82f874
Vaishnavi V (MS/ENC51-BEG) <ffi1cob@bosch.com>
2022-11-18 15:40:13 +0530

    timeout added

--- modal task/modal.js
+++ modal task/modal.js
@@ -15,3 +15,29 @@ window.onclick = function(event) {   // When user clicks anywhere outside of the
     modal.style.display = "none";
   }
 }
+if(localStorage.getItem("count_timer")){   //checking if the key is active 
+  var count_timer = localStorage.getItem("count_timer");
+} else {
+  var count_timer = 60*3; 
+}
+var minutes = parseInt(count_timer/60);
+var seconds = parseInt(count_timer%60);
+function countDownTimer(){
+  if(seconds < 10){
+      seconds= "0"+ seconds ;
+  }if(minutes < 10){
+      minutes= "0"+ minutes ;
+  }
+  
+  document.getElementById("time-left").innerHTML = "Timeout:"+minutes+":"+seconds;
+  if(count_timer <= 0){
+       localStorage.clear("count_timer"); // When timer comes to zero, key gets deleted 
+  } else {
+      count_timer = count_timer -1 ;
+      minutes = parseInt (count_timer/60); // parseInt to get only the first integer
+      seconds = parseInt(count_timer%60);
+      localStorage.setItem("count_timer",count_timer);
+      setTimeout("countDownTimer()",1000);
+  }
+}
+setTimeout("countDownTimer()",1000);
